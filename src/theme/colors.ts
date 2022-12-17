007911b693c013ce33a241d216a7082836b26b85
Jhonatan Lezcano <jhonatanlezcano.plv@gmail.com>
2022-12-17 00:06:31 -0500

    add theme

---
+++ src/theme/colors.ts
@@ -0,0 +1,35 @@
+export const alertColors = {
+  update: '#064789',
+  success: '#388659',
+  danger: '#FF3A3A',
+};
+
+export const lightMode = {
+  dark: false,
+  colors: {
+    primary: '#1d3557',
+    background: '#f0efeb',
+    text: '#2D3436',
+    secondary: '#457b9d',
+    tertiary: '#e63946',
+    textSecondary: '#A4A4A4',
+    shadowColor: '#000',
+    white: '#FFFFFF',
+    alertColors,
+  },
+};
+
+export const darkMode = {
+  dark: true,
+  colors: {
+    primary: '#BB86FC',
+    background: '#16161a',
+    text: '#fffffe',
+    secondary: '#03DAC5',
+    tertiary: '#2cb67d',
+    textSecondary: '#94a1b2',
+    shadowColor: '#ffffff',
+    white: '#FFFFFF',
+    alertColors,
+  },
+};
